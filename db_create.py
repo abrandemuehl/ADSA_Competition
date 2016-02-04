3c83549d6c36b495d09f89f7e94f852b184b3526
Adrian Brandemuehl <abrandemuehl@gmail.com>
2016-02-04 12:07:19 -0600

    Fixed up some links and fixed db scripts to use config properly

--- db_create.py
+++ db_create.py
@@ -1,9 +1,10 @@
 #!flask/bin/python
 from migrate.versioning import api
-from config import SQLALCHEMY_DATABASE_URI
-from config import SQLALCHEMY_MIGRATE_REPO
+from config import current
 from app import db
 import os.path
+SQLALCHEMY_DATABASE_URI = current.SQLALCHEMY_DATABASE_URI
+SQLALCHEMY_MIGRATE_REPO = current.SQLALCHEMY_MIGRATE_REPO
 db.create_all()
 if not os.path.exists(SQLALCHEMY_MIGRATE_REPO):
     api.create(SQLALCHEMY_MIGRATE_REPO, 'database repository')
